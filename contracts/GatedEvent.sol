d28a9a2d984ba352a8c5cd6dfb48708e48987889
Livinalt <livinalt@gmail.com>
2024-09-13 12:23:07 +0100

    Initialising Hardhat

---
+++ contracts/GatedEvent.sol
@@ -0,0 +1,21 @@
+//SPDX-LICENSE-IDENTIFIER: MIT
+pragma solidity 0.8.24;
+
+contract GatedEvent{
+    /* 
+    tis contract is used to manage events. so that 
+    1. a participant needs to mint before they register
+    2. a participant can register for the event
+    3. a participant can choose to not attend/deregister.
+
+     */
+
+     /* 
+     1. we need users to register on the platform
+     2. we need users to create the events for participant to register
+     3. Every event needs to have its own nft
+     4. this nft is what users need to have */
+
+
+
+}
